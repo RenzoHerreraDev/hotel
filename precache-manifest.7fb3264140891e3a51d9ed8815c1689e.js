36958faed855521aa1eb5df4abe4352faae52655
Renzo Herrera <renzo.herrera@softtek.com>
2019-10-18 13:31:47 -0300

    Updates

---
+++ precache-manifest.7fb3264140891e3a51d9ed8815c1689e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fbec9a5e91b9e9dbc977a8a888cdb57",
+    "url": "/hotel/index.html"
+  },
+  {
+    "revision": "982bda096424e07c6d6e",
+    "url": "/hotel/static/css/main.3e90145a.chunk.css"
+  },
+  {
+    "revision": "d1a325624553b1722bdd",
+    "url": "/hotel/static/js/2.cddf8372.chunk.js"
+  },
+  {
+    "revision": "982bda096424e07c6d6e",
+    "url": "/hotel/static/js/main.ff5ebbdf.chunk.js"
+  },
+  {
+    "revision": "3f41df84e365d29fe66a",
+    "url": "/hotel/static/js/runtime-main.9e35a063.js"
+  },
+  {
+    "revision": "aebaec51b2f82a2993fda821ca04e228",
+    "url": "/hotel/static/media/defaultBcg.aebaec51.jpeg"
+  },
+  {
+    "revision": "9f0cccc1e406a5aa9ef6ee36229762c3",
+    "url": "/hotel/static/media/logo.9f0cccc1.svg"
+  }
+]);
